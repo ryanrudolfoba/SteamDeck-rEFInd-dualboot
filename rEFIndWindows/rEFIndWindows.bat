8e6710f0474f5b243fe8e4704d19b7270e7e6778
ryanrudolfoba <98122529+ryanrudolfoba@users.noreply.github.com>
2023-01-18 10:08:23 -0500

    Update rEFIndWindows.bat

--- rEFIndWindows/rEFIndWindows.bat
+++ rEFIndWindows/rEFIndWindows.bat
@@ -25,7 +25,7 @@ echo 2. Right-click the task called rEFIndTask, then select Properties.
 echo 3. Under the General Tab, change the option to RUN WHETHER USER IS LOGGED IN OR NOT.
 echo 4. Put a check mark on DO NOT STORE PASSWORD.
 echo 5. Press OK. Right click the task and select RUN.
-echo 6. Go to C:\1Clover-tools and look for a file called status.txt
+echo 6. Go to C:\1rEFInd-tools and look for a file called status.txt
 echo 7. Open the file and compare the rEFInd GUID and bootsequence they should be the same!
 echo 8. Windows configuration is done!
 pause
